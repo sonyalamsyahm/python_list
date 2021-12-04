3e56fb3c645880e28dd123b5d51dbf66a6b3f9fb
sonyalamsyahm <sonyalamsyahmegadiana92@gmail.com>
2021-12-04 15:02:14 +0700

    update insert.py

---
+++ Insert.py
@@ -0,0 +1,10 @@
+# insert an element to list with specific index
+# example
+vowel = ["a", "i", "u", "o"]
+vowel.insert(3, "e")
+print(vowel)
+
+# another example
+football_players = list(["Messi", "Ronaldo", "Robert L"])
+football_players.insert(2, "Mo. Salah")
+print(football_players)
